8d39b78b61ea8210a5c376a81ef5041ba0e72eac
kingmore96 <wgg9696@163.com>
2018-11-15 16:41:30 +0800

    增加--ip和num互相转换方法

--- ConverTools_g.java
+++ ConverTools_g.java
@@ -73,6 +73,85 @@ public class ConverTools_g {
      * @return
      */
     public static String num2HexIp(Long sim_no){
-        return null;
+        //转换为字符串
+        String s = sim_no.toString();
+
+        //去除首位
+        if( s.length() == 11 ){
+            s = s.substring(1);
+        }
+
+        int temp = 0x08;
+        int hIp = 0;
+        int[] cIp = new int[4];
+
+        //每两位取出来存到int数组中
+        int HIP = Integer.valueOf(s.substring(0,2));
+        hIp = HIP - 30;
+
+        cIp[0] = Integer.valueOf(s.substring(2,4));
+        cIp[1] = Integer.valueOf(s.substring(4,6));
+        cIp[2] = Integer.valueOf(s.substring(6,8));
+        cIp[3] = Integer.valueOf(s.substring(8));
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cIp.length; i++) {
+            //数组中第三个第四个特殊处理
+            if((temp & hIp) != 0){
+                sb.append(Conver_g.byte2HexString((byte)(cIp[i] | 128)));
+            }else{
+                sb.append(Conver_g.byte2HexString((byte)(cIp[i])));
+            }
+            temp = temp >> 1;
+        }
+        return sb.toString().toUpperCase();
+    }
+
+    /**
+     * 16进制字符串转化为sim卡号
+     * eg:0B00809D
+     * sim_no:13311000029
+     * @param hexString
+     * @return
+     */
+    public static Long hexIp2Num(String hexString){
+        int temp = 0x08;
+        int hIp = 3;
+        int[] cIp = new int[4];
+
+        cIp[0] = Integer.valueOf(hexString.substring(0,2),16);
+        cIp[1] = Integer.valueOf(hexString.substring(2,4),16);
+        cIp[2] = Integer.valueOf(hexString.substring(4,6),16);
+        cIp[3] = Integer.valueOf(hexString.substring(6,8),16);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 4; i++) {
+            int t = cIp[i];
+
+            if((temp & hIp) != 0){
+                t = t % 128;
+            }
+
+            if( t < 10 ){
+                sb.append("0"+t);
+            }
+            else{
+                sb.append(t);
+            }
+
+            temp = temp >> 1;
+        }
+        return Long.valueOf(new String("133")+sb.toString());
+    }
+
+    /**
+     * 测试用例
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(ConverTools_g.num2HexIp(new Long("13311000029")));
+        System.out.println(ConverTools_g.hexIp2Num(ConverTools_g.num2HexIp(new Long("13311000029"))));
     }
 }
