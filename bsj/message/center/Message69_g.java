f834d3b87515040552b4ee50cee5bfbc2b5c2bd9
kingmore96 <wgg9696@163.com>
2018-11-21 09:36:26 +0800

    增加69构造函数和测试用例

--- bsj/message/center/Message69_g.java
+++ bsj/message/center/Message69_g.java
@@ -1,6 +1,9 @@
 package bsj.message.center;
 
+import bsj.code.Conver_g;
 import bsj.message.CenterMessage_g;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * 修改UDP的IP和端口
@@ -26,20 +29,55 @@ public class Message69_g extends CenterMessage_g {
         return this.contents;
     }
 
-    //构造ip格式：“xxx.xxx.xxx.xxx”,xxxxx
-    // TODO
-    public Message69_g(String ip,int port){
+    /**
+     * 构造格式，初始化contents
+     * @param ip
+     * @param port
+     * @throws UnsupportedEncodingException
+     */
+    public Message69_g(String ip,int port) throws UnsupportedEncodingException {
         String[] strArr = ip.split("\\.");
         StringBuilder sb = new StringBuilder();
         sb.append("\"");
 
         for (int i = 0; i < strArr.length; i++) {
-
+            String ip1 = strArr[i];
+            sb.append(addZero(ip1,3));
+            if(i < strArr.length - 1){
+                sb.append(".");
+            }
         }
+
+        sb.append("\",");
+        sb.append(port);
+        this.contents = sb.toString().getBytes("US-ASCII");
     }
 
+    /**
+     * 左补0
+     * @param str
+     * @param length
+     * @return
+     */
+    private String addZero(String str, int length) {
+        while(str.length() < length){
+            StringBuilder sb = new StringBuilder();
+            sb.append("0");
+            sb.append(str);
+            str = sb.toString();
+        }
+        return str;
+    }
 
-
-
-
+    /**
+     * 测试用例
+     * @param args
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        Message69_g message69_g = new Message69_g("219.232.191.073",8890);
+        String s = Conver_g.bytes2HexString(message69_g.getContents());
+        System.out.println(s);
+        String s1 = Conver_g.bytes2HexString(message69_g.buildMessage());
+        System.out.println(s1);
+    }
 }
